9385d8219d1cdaa96a76ce4988184abd3dbb4947
N103Gremillion <nathan103grem@gmail.com>
2025-03-06 00:23:09 -0600

    got basic layout done for debugger

--- debugger.cpp
+++ debugger.cpp
@@ -24,31 +24,74 @@ void draw_instructions(Debugger& debugger) {
 
 void draw_keys(Debugger& debugger) {
   // reset location of key to correct starting value
-  debugger.key.x = 210;
+  debugger.key.x = 200;
   debugger.key.y = 0;
-  SDL_Rect text_rect = {210, 0, debugger.key.w, debugger.key.h};
+  SDL_Rect text_rect = {200, 0, debugger.key.w / 2, debugger.key.h / 2};
   const char* keys[] = {"1", "2", "3", "4", "Q", "W", "E", "R", "A", "S", "D", "F", "Z", "X", "C", "V"};
 
   for (int i = 0; i < debugger.num_of_keys; i++) {
-    
     if (i % 4 == 0 && i != 0) {
       debugger.key.y += debugger.key.h;
-      debugger.key.x = 210;
+      text_rect.y += debugger.key.h;
+      debugger.key.x = 200;
+      text_rect.x = 200;
     } else if (i != 0){
       debugger.key.x += debugger.key.w;
+      text_rect.x += debugger.key.w;
     }
     SDL_Surface* text_surface = TTF_RenderText_Solid(debugger.font, keys[i], debugger.text_color);
     SDL_Texture* text_texture = SDL_CreateTextureFromSurface(debugger.render, text_surface); 
     SDL_RenderDrawRect(debugger.render, &debugger.key);
-    SDL_RenderCopy(debugger.render, text_texture, NULL, &debugger.key);
+    SDL_RenderCopy(debugger.render, text_texture, NULL, &text_rect);
     SDL_FreeSurface(text_surface);
     SDL_DestroyTexture(text_texture);
   }
 }
 
+void draw_registers(Debugger& debugger) {
+  debugger.u8_reg.x = 400;
+  debugger.u8_reg.y = 0;
+  // char* text = "V: E693";
+  // render the general Vx registers first
+  for (int i = 0; i < debugger.num_of_general_registers; i++){
+    if (i % 3 == 0 && i != 0){
+      debugger.u8_reg.x = 400;
+      debugger.u8_reg.y += debugger.u8_reg.h;
+    } else if (i != 0){
+      debugger.u8_reg.x += debugger.u8_reg.w;
+    }
+    SDL_RenderDrawRect(debugger.render, &debugger.u8_reg);
+  }
+  // render the remaning specific registers
+  SDL_Rect u16_reg = {debugger.u8_reg.x + debugger.u8_reg.w, debugger.u8_reg.y, debugger.u8_reg.w * 2, debugger.u8_reg.h};
+
+  // pc register
+  SDL_RenderDrawRect(debugger.render, &u16_reg);
+
+  u16_reg.y += u16_reg.h;
+
+  // sp register
+  SDL_RenderDrawRect(debugger.render, &u16_reg);
+
+  u16_reg.y += u16_reg.h;
+
+  // I register
+  SDL_RenderDrawRect(debugger.render, &u16_reg);
+
+  // remaining special u8 registers
+  // delay timer
+  debugger.u8_reg.y += debugger.u8_reg.h;
+  SDL_RenderDrawRect(debugger.render, &debugger.u8_reg);
+
+  // sound timer
+  debugger.u8_reg.y += debugger.u8_reg.h;
+  SDL_RenderDrawRect(debugger.render, &debugger.u8_reg);
+}
+
 // draws current state of all components in debugger
 void render_debugger(Debugger& debugger) {
   fill_background(debugger);
+  draw_registers(debugger);
   draw_instructions(debugger);
   draw_keys(debugger);
   SDL_RenderPresent(debugger.render);
--- debugger.hpp
+++ debugger.hpp
@@ -18,6 +18,7 @@ struct Debugger;
 void fill_background(Debugger& debugger);
 void draw_instructions(Debugger& debugger);
 void draw_keys(Debugger& debugger);
+void draw_registers(Debugger& debugger);
 void setup_fonts(Debugger& debugger);
 void render_debugger(Debugger& debugger);
 void init_debugger(Debugger& debugger);
@@ -26,17 +27,21 @@ typedef struct Debugger{
 
   int x = SDL_WINDOWPOS_CENTERED;
   int y = SDL_WINDOWPOS_CENTERED;
-  int width = 400;
+  int width = 700;
   int height = 200;
 
   // for the instruction part left side
-  SDL_Rect instruction = {0, 0, 195, 50}; // x=0, y=0, w=200, h=50
+  SDL_Rect instruction = {0, 0, 200, 50}; // x=0, y=0, w=200, h=50
   int num_of_instruction = 4;
 
-  // for the keys part right side
-  SDL_Rect key = {210, 0, 45, 50}; // x=200, y=0, w=50, h=50
+  // for the keys middle part
+  SDL_Rect key = {200, 0, 50, 50}; // x=200, y=0, w=50, h=50
   int num_of_keys = 16;
   
+  // for the registers right side
+  // SDL_Rect register = {};
+  SDL_Rect u8_reg = {400, 0, 100, 25};
+  int num_of_general_registers = 16; // for the 16 general register note: still have to add the special ones
 
   // screen (represented by 64*32 pixel screen
   SDL_Window* window = nullptr;
