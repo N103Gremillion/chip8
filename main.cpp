eb83916f6daae851ac2756163409d01a01cdeaa3
N103Gremillion <nathan103grem@gmail.com>
2025-03-10 16:14:57 -0500

    fixed a few more buggs

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include "chip8.hpp"
 #include <SDL2/SDL.h>
 
-#define ROM "./game_roms/RPS.ch8"
+string games[5] = {"./game_roms/snake.ch8", "./game_roms/octopeg.ch8"};
+string demo[5] = {"./demo_roms/1dcell.ch8", "./demo_roms/octojam2title.ch8", "./demo_roms/Zero.ch8", "./demo_roms/octojam5title.ch8"};
 
 bool debug_mod = false;
 
 int main() {
     Chip8 chip;    
-    load_rom(string(ROM), chip);
+    load_rom(string(demo[3]), chip);
     run(chip, debug_mod);
     free_chip(chip);
 }   
